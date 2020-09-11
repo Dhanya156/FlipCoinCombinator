31b7cfae0b6d9278c88df61403e3fc0762e39164
Dhanya156 <dhanyavirupaksha@gmail.com>
2020-09-11 06:41:49 -0700

    Added:Displaying Percentage Of Singlet Combinantion

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -2,10 +2,36 @@
 
 echo "WELCOME TO FLIP COIN COMBINATOR"
 
+declare -A headTail
+times=0
+
+while [ $times -lt 100 ]
+do
 randomNum=$(( RANDOM % 2  ))
+
+
 if [ $randomNum -eq 1 ]
 then
-        echo "HEAD"
+        val=${headTail["Head"]}
+        val=$(($val+1))
+
+        headTail["Head"]=$val
 else
-        echo "TAIL"
+        val=${headTail["Tail"]}
+        val=$(($val+1))
+
+        headTail["Tail"]=$val
+
 fi
+ times=$(($times+1))
+done
+
+headC=${headTail["Head"]}
+tailC=${headTail["Tail"]}
+
+headPer=$((100*$headC/$times))
+
+tailPer=$((100*$tailC/$times))
+
+echo "Head % is : $headPer"
+echo "Tail % is : $tailPer"
